3b474a12cb893c828f06e873b8794a0ff8bd5b36
= <gab.dark.100@gmail.com>
2016-04-02 15:32:15 -0300

    update

--- MMX4TextTool/runsdvui.cmd
+++
@@ -1,2 +0,0 @@
-cd /d "C:\Users\gabriel\Documents\Visual Studio 2015\Projects\MMX4TextTool\MMX4TextTool" &msbuild "MMX4TextTool.csproj" /t:sdvViewer /p:configuration="Debug" /p:platform=Any CPU
-exit %errorlevel% 
